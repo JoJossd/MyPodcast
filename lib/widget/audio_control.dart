be2c5b76fd6d5bba8fefdd6582482c2c26a63973
JoJo <zx910819zx@163.com>
2020-03-24 14:09:44 +0800

    finish basic audio control funtion

--- lib/widget/audio_control.dart
+++ lib/widget/audio_control.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:provider/provider.dart';
-
 import 'package:my_podcast/model/rssfeed_data.dart';
+
+// TODO: set volumn
+enum PlayerState { stopped, playing, paused }
 
 class AudioControl extends StatefulWidget {
   @override
@@ -13,78 +16,98 @@ class AudioControl extends StatefulWidget {
 
 class _AudioControlState extends State<AudioControl> {
   // AudioCache audioCache = AudioCache();
-  AudioPlayer audioPlayer = AudioPlayer();
+  AudioPlayer audioPlayer;
+  AudioPlayerState _audioPlayerState;
 
-  bool _isPlaying = false;
-  bool _isStarted = false;
+  StreamSubscription<Duration> _positionSubscription;
+  StreamSubscription<Duration> _durationSubscription;
+  StreamSubscription<AudioPlayerState> _playerStateSubscription;
+  StreamSubscription<void> _playerCompleteSubscription;
+  StreamSubscription<String> _playerErrorSubscription;
+
   Duration _duration;
-  Duration _playPosition;
-  int time;
-  String timeLeft = "";
-  double progress = 0.0;
+  Duration _position;
 
-  // set the initial value of some variables when the app lunchs.
+  PlayerState _playerState = PlayerState.stopped;
+
+  get _isPlaying => _playerState == PlayerState.playing;
+  // get _isPaused => _playerState == PlayerState.paused;
+  get _durationText => _duration?.toString()?.split('.')?.first ?? '';
+  get _positionText => _position?.toString()?.split('.')?.first ?? '';
+
   @override
   void initState() {
     super.initState();
+    audioPlayer = AudioPlayer();
 
-    audioPlayer.onAudioPositionChanged.listen((Duration p) async {
-      print('Current position: $p');
-      time = await audioPlayer.getDuration();
-      _duration = p;
-      if (_duration == null) {
-        timeLeft = "Time Left 0s/0s";
-      } else {
-        timeLeft = "Time Left ${_duration.inSeconds}s/${time / 1000}s";
-      }
-      if (time == null || _duration == null) {
-        progress = 0.0;
-      } else {
-        progress = (_duration.inMilliseconds) / time;
-      }
-      print(progress);
-      setState(() {});
+    _durationSubscription = audioPlayer.onDurationChanged.listen((duration) {
+      setState(() => _duration = duration);
     });
 
-    audioPlayer.onPlayerStateChanged.listen((AudioPlayerState state) {
-      print("$state");
-      if (state == AudioPlayerState.PLAYING) {
-        setState(() {
-          _isPlaying = true;
-        });
-      } else {
-        if (mounted) {
-          setState(() {
-            _isPlaying = false;
-          });
-        }
-      }
+    _positionSubscription =
+        audioPlayer.onAudioPositionChanged.listen((Duration p) async {
+      print('Current position: $p');
+      setState(() => _position = p);
+    });
+
+    _playerStateSubscription = audioPlayer.onPlayerStateChanged.listen((state) {
+      if (!mounted) return;
+      print('Current state: $state');
+      setState(() => _audioPlayerState = state);
+    });
+
+    _playerCompleteSubscription =
+        audioPlayer.onPlayerCompletion.listen((event) {
+      setState(() {
+        _playerState = PlayerState.stopped;
+        _position = _duration;
+      });
+    });
+
+    _playerErrorSubscription = audioPlayer.onPlayerError.listen((msg) {
+      print('audioPlayer error : $msg');
+      setState(() {
+        _playerState = PlayerState.stopped;
+        _duration = Duration(seconds: 0);
+        _position = Duration(seconds: 0);
+      });
     });
   }
 
   Future<void> _play(String url) async {
-    if (!_isStarted) {
-      await audioPlayer.play(url);
-      setState(() => _isStarted = true);
-    } else
-      await audioPlayer.resume();
-
-    // setState(() => _isPlaying = true);
+    final playPosition = (_position != null &&
+            _duration != null &&
+            _position.inMilliseconds > 0 &&
+            _position.inMilliseconds < _duration.inMilliseconds)
+        ? _position
+        : null;
+    final result = await audioPlayer.play(url, position: playPosition);
+    if (result == 1) {
+      setState(() => _playerState = PlayerState.playing);
+    }
+    audioPlayer.setPlaybackRate(playbackRate: 1.0);
   }
 
   Future<void> _pause() async {
-    await audioPlayer.pause();
-    // setState(() => _isPlaying = false);
+    final result = await audioPlayer.pause();
+    if (result == 1) {
+      setState(() => _playerState = PlayerState.paused);
+    }
   }
 
-  // Future<void> _slideJump(double _newPosition) async {
-  //   String result = await _sound.seekToPlayer(_newPosition.toInt());
-  //   setState(() => _playPosition = _newPosition);
-  // }
+  Future<void> _jumpTo(double v) async {
+    final newPosition = v * _duration.inMilliseconds;
+    await audioPlayer.seek(Duration(milliseconds: newPosition.toInt()));
+  }
+
   @override
-  void dispose() async {
-    await audioPlayer.release();
-    await audioPlayer.dispose();
+  void dispose() {
+    audioPlayer.stop();
+    _durationSubscription?.cancel();
+    _positionSubscription?.cancel();
+    _playerCompleteSubscription?.cancel();
+    _playerErrorSubscription?.cancel();
+    _playerStateSubscription?.cancel();
     super.dispose();
   }
 
@@ -92,29 +115,63 @@ class _AudioControlState extends State<AudioControl> {
   Widget build(BuildContext context) {
     final item = Provider.of<Podcast>(context).selectedItem;
     return SizedBox(
-        width: 300,
-        height: 120,
-        child: Column(
-          children: <Widget>[
-            // TODO: implement draggable controller
-            Slider.adaptive(
-              value: 0,
-              onChanged: null,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  icon: _isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
-                  iconSize: Theme.of(context).iconTheme.size,
-                  color: Colors.black,
-                  onPressed: () {
-                    _isPlaying ? _pause() : _play(item.enclosure.url);
-                  },
-                ),
-              ],
-            ),
-          ],
+        width: 400,
+        height: 130,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(28, 0, 28, 0),
+          child: Column(
+            children: <Widget>[
+              Slider.adaptive(
+                value: (_position != null &&
+                        _duration != null &&
+                        _position.inMilliseconds > 0 &&
+                        _position.inMilliseconds < _duration.inMilliseconds)
+                    ? _position.inMilliseconds / _duration.inMilliseconds
+                    : 0.0,
+                onChanged: (v) => _jumpTo(v),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Text(
+                    _position != null ? '${_positionText ?? ''}' : '',
+                  ),
+                  Text(
+                    _position != null
+                        ? '${_durationText ?? ''}'
+                        : _duration != null ? _durationText : '',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.fast_rewind),
+                    iconSize: Theme.of(context).iconTheme.size,
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon:
+                        _isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+                    iconSize: Theme.of(context).iconTheme.size,
+                    color: Colors.black,
+                    onPressed: () {
+                      _isPlaying ? _pause() : _play(item.enclosure.url);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.fast_forward),
+                    iconSize: Theme.of(context).iconTheme.size,
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
         ));
   }
 }
